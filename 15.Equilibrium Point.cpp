93215669cec1f006d5f6feff2be110421dd20f6a
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-25 07:24:50 +0530

    Create 15.Equilibrium Point.cpp

---
+++ 15.Equilibrium Point.cpp
@@ -0,0 +1,10 @@
+/* Given an array arr of non-negative numbers. The task is to find the first equilibrium point
+in an array. The equilibrium point in an array is an index (or position) such that the sum of 
+all elements before that index is the same as the sum of elementsafter it.*/
+
+
+//LEVEL--
+//CONCEPT--
+
+
+//SOLUTION----
