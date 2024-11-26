b475f4b439a6ea060fb8ce678ddb47f5005893e7
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-26 08:01:32 +0530

    Update 16.Peak element.cpp

--- 16.Peak element.cpp
+++ 16.Peak element.cpp
@@ -1,1 +1,6 @@
+/*Given an 0-indexed array of integers arr[], find its peak element and return its index.
+An element is considered to be peak if its value is greater than or equal to the values of its
+adjacent elements (if they exist).*/
 
+
+//LEVE--
