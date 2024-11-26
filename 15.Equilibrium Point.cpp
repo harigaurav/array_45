a217a83953d7e6a0231f59e50446cc91a52c3bcc
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-26 07:55:45 +0530

    Update 15.Equilibrium Point.cpp

--- 15.Equilibrium Point.cpp
+++ 15.Equilibrium Point.cpp
@@ -3,8 +3,28 @@ in an array. The equilibrium point in an array is an index (or position) such th
 all elements before that index is the same as the sum of elementsafter it.*/
 
 
-//LEVEL--
-//CONCEPT--
+//LEVEL--EASY*****
+//CONCEPT--LEFT_SUM/RIGHT_SUM
 
 
-//SOLUTION----
+//SOLUTION----class Solution {
+  public:
+    int equilibriumPoint(vector<long long> &arr) {
+        // Your code herea
+        long long total_sum=0;
+        long long n =arr.size();
+        for(int i=0;i<n;i++){
+            total_sum+=arr[i];
+        }
+        long long left_sum=0,right_sum=total_sum;
+        for(int i=0;i<n;i++){
+            right_sum-=arr[i];
+        
+        if(right_sum==left_sum){
+            return i+1;
+        }
+        left_sum+=arr[i];
+        }
+        return -1;
+    }
+};
