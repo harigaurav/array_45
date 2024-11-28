17c2e3565241f53572becea7c7cacea3f5c2135c
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-28 07:29:18 +0530

    Update 30.container with most water.cpp

--- 30.container with most water.cpp
+++ 30.container with most water.cpp
@@ -1,1 +1,11 @@
+/*Given non-negative integers arr1,arr2,....arrn where each represents a point at
+coordinate (i, arri). For each i vertical lines are drawn such that the two endpoints of line i
+is at (i, arri) and (i,0). Find two lines, which together with x-axis form a container, such that
+it contains the most water.*/
 
+
+//LEVEL--
+//CONCEPT--
+
+
+//SOLUTION 
