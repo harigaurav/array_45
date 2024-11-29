6cb449ae0f49ef7dfcdf7e47e692db380d5a997d
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-29 06:32:40 +0530

    Update 31.max triplet product.cpp

--- 31.max triplet product.cpp
+++ 31.max triplet product.cpp
@@ -1,1 +1,24 @@
+/*Given an array arr of size n, the task is to find the maximum triplet product in the array.*/
+
+
+
+//LEVEL- EASY****
+//CONCEPT-SIMPLE OBSERVATION 
+
+
+
+//SOLUTION---
+//User function Template for C++
+
+class Solution {
+  public:
+    long long maxTripletProduct(long long arr[], int n)
+    {
+    	// Complete the function
+    	sort(arr,arr+n);
+    	long long num1=arr[n-1]*arr[n-2]*arr[n-3];
+    	long long num2= arr[0]*arr[1]*arr[n-1];
+    	return max(num1,num2);
+    }
+};
 
