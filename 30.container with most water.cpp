269b6c7b577b9ba163e6d71a193a80dad5ab9ef9
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-11-29 06:25:56 +0530

    Update 30.container with most water.cpp

--- 30.container with most water.cpp
+++ 30.container with most water.cpp
@@ -4,8 +4,30 @@ is at (i, arri) and (i,0). Find two lines, which together with x-axis form a con
 it contains the most water.*/
 
 
-//LEVEL--
-//CONCEPT--
+//LEVEL--medium 
+//CONCEPT--TWO POINTERS 
 
 
-//SOLUTION 
+//SOLUTION ---
+//User function template for C++
+
+long long maxArea(long long A[], int len)
+{
+    // Your code goes here
+    int i=0;
+    long long water;
+    int j=len-1;
+    long long ans=0;
+    while(i<j){
+        water=(j-i)*min(A[i],A[j]);
+            if(A[i]<A[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+            ans=max(ans,water);
+    }
+        return ans;
+    
+}
